ae0982aa4ea2415569e03a6ce5b4358991859e89
Ornithologist Coder <ornithocoder@users.noreply.github.com>
2017-04-13 10:03:35 +0200

    Runs tests on Linux

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -2,5 +2,15 @@ import XCTest
 @testable import MastodonKitTests
 
 XCTMain([
-    testCase(MastodonKitTests.allTests),
+    testCase(AccountsTests.allTests),
+    testCase(BlocksTests.allTests),
+    testCase(FavouritesTests.allTests),
+    testCase(FollowRequestsTests.allTests),
+    testCase(InstancesTests.allTests),
+    testCase(MutesTests.allTests),
+    testCase(NotificationsTests.allTests),
+    testCase(ReportsTests.allTests),
+    testCase(SearchTests.allTests),
+    testCase(StatusesTests.allTests),
+    testCase(TimelinesTests.allTests)
 ])
