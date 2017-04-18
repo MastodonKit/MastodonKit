7b1ff373c27e559494aab36271a9926af88f3416
Ornithologist Coder <ornithocoder@users.noreply.github.com>
2017-04-18 17:20:04 +0200

    Improves code coverage

--- Tests/MastodonKitTests/MastodonClientTests.swift
+++ Tests/MastodonKitTests/MastodonClientTests.swift
@@ -2,4 +2,23 @@ import XCTest
 @testable import MastodonKit
 
 class MastodonClientTests: XCTestCase {
+    func testMastodonClientInitializationWithAccessToken() {
+        let client = MastodonClient(baseURL: "https://my.mastodon.instance/", accessToken: "foo")
+
+        XCTAssertNotNil(client)
+    }
+
+    func testMastodonClientInitializationWithoutAccessToken() {
+        let client = MastodonClient(baseURL: "https://my.mastodon.instance/")
+
+        XCTAssertNotNil(client)
+    }
+
+    func testMastodonClientAccessTokenConfiguration() {
+        let client = MastodonClient(baseURL: "https://my.mastodon.instance/")
+
+        XCTAssertNil(client.accessToken)
+        client.accessToken = "bar"
+        XCTAssertEqual(client.accessToken, "bar")
+    }
 }
