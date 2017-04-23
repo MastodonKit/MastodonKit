0e9a662459387c65e284eee2a16851ecdbffa11a
Ornithologist Coder <ornithocoder@users.noreply.github.com>
2017-04-23 09:56:36 +0200

    Adds Notifications's dismissAll() and dismiss(:)

--- Sources/MastodonKit/Resources/Notifications.swift
+++ Sources/MastodonKit/Resources/Notifications.swift
@@ -15,4 +15,19 @@ public struct Notifications {
     public static func notification(id: Int) -> NotificationResource {
         return NotificationResource(path: "/api/v1/notifications/\(id)", parse: NotificationResource.parser)
     }
+
+    /// Deletes all notifications for the authenticated user.
+    ///
+    /// - Returns: Resource for `Notification`.
+    public static func dismissAll() -> NotificationResource {
+        return NotificationResource(path: "/api/v1/notifications/clear", method: .post, parse: NotificationResource.parser)
+    }
+
+    /// Deletes a single notification for the authenticated user.
+    ///
+    /// - Parameter id: The notification id.
+    /// - Returns: Resource for `Notification`.
+    public static func dismiss(id: Int) -> NotificationResource {
+        return NotificationResource(path: "/api/v1/notifications/dismiss/\(id)", method: .post, parse: NotificationResource.parser)
+    }
 }
--- Tests/MastodonKitTests/Resources/NotificationsTests.swift
+++ Tests/MastodonKitTests/Resources/NotificationsTests.swift
@@ -21,4 +21,24 @@ class NotificationsTests: XCTestCase {
 
         XCTAssertTrue(type(of: resource.parse) == ParserFunctionType<MastodonKit.Notification?>.self)
     }
+
+    func testDismissAll() {
+        let resource = Notifications.dismissAll()
+
+        XCTAssertEqual(resource.path, "/api/v1/notifications/clear")
+        XCTAssertEqual(resource.httpMethod, .post)
+        XCTAssertNil(resource.parameters)
+
+        XCTAssertTrue(type(of: resource.parse) == ParserFunctionType<MastodonKit.Notification?>.self)
+    }
+
+    func testDismissWithID() {
+        let resource = Notifications.dismiss(id: 42)
+
+        XCTAssertEqual(resource.path, "/api/v1/notifications/dismiss/42")
+        XCTAssertEqual(resource.httpMethod, .post)
+        XCTAssertNil(resource.parameters)
+
+        XCTAssertTrue(type(of: resource.parse) == ParserFunctionType<MastodonKit.Notification?>.self)
+    }
 }
