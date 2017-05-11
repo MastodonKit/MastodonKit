001cccc58c56b3af876c201fd372b50df6d89321
Ornithologist Coder <ornithocoder@users.noreply.github.com>
2017-05-11 15:37:10 +0200

    Adds description to ClientError

--- Sources/MastodonKit/ClientError.swift
+++ Sources/MastodonKit/ClientError.swift
@@ -1,7 +1,10 @@
 import Foundation
 
 public enum ClientError: Error {
+    /// Failed to build the URL to make the request.
     case malformedURL
+    /// The Mastodon service returned an error.
     case mastodonError(String)
+    /// Failed to parse the response from Mastodon.
     case dataError
 }
