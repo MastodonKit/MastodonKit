c007021259f0903271e32f7ee0fb86d71d25839c
Ornithologist Coder <ornithocoder@users.noreply.github.com>
2018-01-02 01:17:18 +0100

    Adds List model

---
+++ Sources/MastodonKit/Models/List.swift
@@ -0,0 +1,16 @@
+//
+//  List.swift
+//  MastodonKit
+//
+//  Created by Ornithologist Coder on 1/2/18.
+//  Copyright © 2018 MastodonKit. All rights reserved.
+//
+
+import Foundation
+
+public class List: Codable {
+    /// The ID of the list.
+    public let id: String
+    /// The Title of the list.
+    public let title: String
+}
