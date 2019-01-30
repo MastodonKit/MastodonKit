f1fcd69097da03d4d623ea873662004b35700b85
Bruno Philipe <git@bruno.ph>
2019-01-31 00:13:03 +0100

    Update Card entity and add its reference to Status

--- Sources/MastodonKit/Models/Card.swift
+++ Sources/MastodonKit/Models/Card.swift
@@ -9,12 +9,50 @@
 import Foundation
 
 public class Card: Codable {
-    /// The url associated with the card.
-    public let url: URL
-    /// The title of the card.
-    public let title: String
-    /// The card description.
-    public let description: String
-    /// The image associated with the card, if any.
-    public let image: URL?
+	/// The url associated with the card.
+	public let url: URL
+	/// The title of the card.
+	public let title: String
+	/// The card description.
+	public let description: String
+	/// The image associated with the card, if any.
+	public let imageUrl: URL?
+	/// The type of the card's contents.
+	public let type: Type?
+	/// The name of the card content's author.
+	public let authorName: String?
+	/// The URL for the card content's author.
+	public let authorUrl: URL?
+	/// The name of the card content's provider.
+	public let providerName: String?
+	/// The URL for the card content's provider.
+	public let providerUrl: URL?
+	/// The card contents in HTML.
+	public let html: String?
+	/// The card's content width.
+	public let width: Int?
+	/// The card's content height.
+	public let height: Int?
+
+	private enum CodingKeys: String, CodingKey {
+		case url
+		case title
+		case description
+		case imageUrl = "image"
+		case type
+		case authorName = "author_name"
+		case authorUrl = "author_url"
+		case providerName = "provider_name"
+		case providerUrl = "provider_url"
+		case html
+		case width
+		case height
+	}
+
+    public enum `Type`: String, Codable {
+        case link
+        case photo
+        case video
+        case rich
+    }
 }
--- Sources/MastodonKit/Models/Status.swift
+++ Sources/MastodonKit/Models/Status.swift
@@ -55,6 +55,8 @@ public class Status: Codable {
     public let reblog: Status?
     /// Whether this is the pinned status for the account that posted it.
     public let pinned: Bool?
+    /// A content card with linked content.
+    public let card: Card?
 
     private enum CodingKeys: String, CodingKey {
         case id
@@ -80,5 +82,6 @@ public class Status: Codable {
         case language
         case reblog
         case pinned
+        case card
     }
 }
