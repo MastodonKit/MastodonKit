7566124fe0cb3dc3a2aaa60bbc901d735a6e1dfb
Daniel Nitsikopoulos <daniel.nitsikopoulos@icelab.com.au>
2019-07-26 18:59:29 +1000

    Add support for Polls requests

--- Sources/MastodonKit/Requests/Polls.swift
+++ Sources/MastodonKit/Requests/Polls.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+/// `Polls` requests.
+public enum Polls {
+    /// Fetches a poll.
+    ///
+    /// - Parameter id: The poll id.
+    /// - Returns: Request for `Poll`.
+    public static func poll(id: String) -> Request<Poll> {
+        return Request<Poll>(path: "/api/v1/polls/\(id)")
+    }
+    
+    /// Votes on a poll.
+    ///
+    /// - Parameters:
+    ///   - choices: Array of choice indices.
+    /// - Returns: Request for `Poll`.
+    public static func vote(id: String,
+                              choices: [String] = []) -> Request<Poll> {
+        
+        let parameters = choices.map(toArrayOfParameters(withName: "choices"))
+        
+        let method = HTTPMethod.post(.parameters(parameters))
+        return Request<Poll>(path: "/api/v1/polls/\(id)/vote", method: method)
+    }
+}
