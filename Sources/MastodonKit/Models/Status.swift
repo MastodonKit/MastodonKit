7bf0a4a0768d3c29e263256ec7c6a8e744976386
Marcel Voss <me@marcelvoss.com>
2022-11-10 15:56:20 +0100

    Add Card to response of statuses

--- Sources/MastodonKit/Models/Status.swift
+++ Sources/MastodonKit/Models/Status.swift
@@ -55,6 +55,8 @@ public class Status: Codable {
     public let reblog: Status?
     /// Whether this is the pinned status for the account that posted it.
     public let pinned: Bool?
+    /// Preview card for links included within status content.
+    public let card: Card?
 
     private enum CodingKeys: String, CodingKey {
         case id
@@ -80,5 +82,6 @@ public class Status: Codable {
         case language
         case reblog
         case pinned
+        case card
     }
 }
